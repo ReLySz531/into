5fd09f8bf42de1682728353603799694e126b97c
ReLySz531 <ReLySz531@yahoo.com>
2023-02-03 10:29:43 -0500

    commit

---
+++ js intro.js
@@ -0,0 +1,15 @@
+var firstName = "Renee";
+
+var lastName = "Szatkiewicz";
+
+// String
+// "Renee" "Szatkiewicz"
+
+var age = 33;
+
+alert("My name is Renee Szatkiewicz");
+
+alert("I am 33 years old");
+
+
+console.log("I am actually having fun learning about how to use Javascript");
